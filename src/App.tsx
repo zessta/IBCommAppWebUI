6e1964782d4237f22222f99b5507f3b65c13365c
zameer.shaik <zameer.shaik@zessta.com>
2025-03-07 12:24:26 +0530

    initial-setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div style={{ fontWeight: "bold", fontSize: "20px", textAlign: "center" }}>
+      IB - Communication
+    </div>
+  );
+};
+
+export default App;
